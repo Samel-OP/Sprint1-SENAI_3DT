09e2cf84a5949bf74b3340e62b5ce752938742c5
Samuel De Oliveira Pereira <47429258824@info.scs>
2022-02-09 17:19:59 -0300

    Criptografia criada

--- OCR-Projeto/2022-3T-ProjetoBase-Backend/Patrimonio/Repositories/UsuarioRepository.cs
+++ OCR-Projeto/2022-3T-ProjetoBase-Backend/Patrimonio/Repositories/UsuarioRepository.cs
@@ -1,6 +1,7 @@
 ﻿using Patrimonio.Contexts;
 using Patrimonio.Domains;
 using Patrimonio.Interfaces;
+using Patrimonio.Utils;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,6 +21,23 @@ namespace Patrimonio.Repositories
 
         public Usuario Login(string email, string senha)
         {
+            var usuario = ctx.Usuarios.FirstOrDefault(u => u.Email == email);
+            var complt = ctx.Usuarios.FirstOrDefault(s => s.Senha == senha);
+
+            if (complt.Senha.Length < 32 && complt.Senha[0] != '$')
+            {
+                complt.Senha = Criptografia.GerarHash(usuario.Senha);
+
+                ctx.SaveChanges();
+
+                if (usuario != null)
+                {
+                    bool confere = Criptografia.Comparar(senha, usuario.Senha);
+                    if (confere)
+                        return usuario;
+                }
+            }
+
             return ctx.Usuarios.FirstOrDefault(u => u.Email == email && u.Senha == senha);
         }
     }
